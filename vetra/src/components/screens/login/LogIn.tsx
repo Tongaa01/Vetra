566fb69e63e7f0e15ea718e3814e841441be3b66
Tonga <gaston.astudillo@gmail.com>
2025-06-09 22:23:05 -0300

    arreglos link imagen login y signin

--- vetra/src/components/screens/login/LogIn.tsx
+++ vetra/src/components/screens/login/LogIn.tsx
@@ -1,83 +1,83 @@
-import styles from "./Login.module.css"
 import { useEffect, useState, type ChangeEvent } from "react"
 import { useNavigate } from "react-router-dom"
-import { Button } from "../../ui/Button/Button"
-import type { ILoginBody } from "../../../types/ILoginBody"
 import { authRequest } from "../../../http/auth/authRequest"
 import { tokenIsExpired } from "../../../services/jwtService"
+import type { ILoginBody } from "../../../types/ILoginBody"
+import { Button } from "../../ui/Button/Button"
 import { Footer } from "../../ui/Footer/Footer"
+import styles from "./Login.module.css"
 
 
 
 const initialValues = {
-  email: "",
-  password: ""
+    email: "",
+    password: ""
 }
 export const Login = () => {
-  const [logInInfo, setLogInInfo] = useState<ILoginBody>(initialValues)
-  const [errorMessage, setErrorMessage] = useState<string>("")
+    const [logInInfo, setLogInInfo] = useState<ILoginBody>(initialValues)
+    const [errorMessage, setErrorMessage] = useState<string>("")
 
-  const handleChangeInputs = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.target
-    setLogInInfo((prev) => ({ ...prev, [`${name}`]: value }))
-  }
-  const navigate = useNavigate()
-  const handleLogin = async () => {
-    const token = await authRequest(logInInfo)
-    if (token) {
-      localStorage.setItem('token', token)
-      navigate("/")
-      return
+    const handleChangeInputs = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = event.target
+        setLogInInfo((prev) => ({ ...prev, [`${name}`]: value }))
     }
-    setErrorMessage("Contraseña o Email invalidos")
-  }
-
-
-  useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (token && !tokenIsExpired(token)) {
-      navigate("/")
-    } else {
-      localStorage.clear()
+    const navigate = useNavigate()
+    const handleLogin = async () => {
+        const token = await authRequest(logInInfo)
+        if (token) {
+            localStorage.setItem('token', token)
+            navigate("/")
+            return
+        }
+        setErrorMessage("Contraseña o Email invalidos")
     }
-  }, [])
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (token && !tokenIsExpired(token)) {
+            navigate("/")
+        } else {
+            localStorage.clear()
+        }
+    }, [])
 
 
 
-  return (
-    <div className={styles.mainContainer}>
-      <div className={styles.contentContainer}>
-        <div className={styles.imageContainer}>
-          <img className={styles.image} src="public\vetra_banner.png" />
-        </div>
-        <div className={styles.logInComponent}>
-          <div className={styles.logInComponent_content}>
-            {errorMessage !== "" ? <div className={styles.errorMessage}>{errorMessage}</div> : <div></div>}
-            <div className={styles.logInComponent_content_inputs}>
-              <div className={styles.inputComponent}>
-                <p>Email</p>
-                <input placeholder="example@gmail.com" className={styles.inpuntStyles} type="text" name="email" onChange={handleChangeInputs} />
-              </div>
-              <div className={styles.inputComponent}>
-                <p>Contraseña</p>
-                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="password" onChange={handleChangeInputs} />
-              </div>
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.contentContainer}>
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src="https://res.cloudinary.com/danzaburou/image/upload/v1749489285/vetra_banner_wflubd.png" />
+                </div>
+                <div className={styles.logInComponent}>
+                    <div className={styles.logInComponent_content}>
+                        {errorMessage !== "" ? <div className={styles.errorMessage}>{errorMessage}</div> : <div></div>}
+                        <div className={styles.logInComponent_content_inputs}>
+                            <div className={styles.inputComponent}>
+                                <p>Email</p>
+                                <input placeholder="example@gmail.com" className={styles.inpuntStyles} type="text" name="email" onChange={handleChangeInputs} />
+                            </div>
+                            <div className={styles.inputComponent}>
+                                <p>Contraseña</p>
+                                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="password" onChange={handleChangeInputs} />
+                            </div>
+                        </div>
+                        <div className={styles.logInComponent_content_button}>
+                            <Button text="Iniciar Sesion" action={handleLogin} styleSet={true} />
+                        </div>
+
+                    </div>
+                    <div onClick={() => {
+                        navigate("/signin")
+                    }}>
+                        <p className={styles.navText}>No tengo cuenta</p>
+                    </div>
+                </div>
             </div>
-            <div className={styles.logInComponent_content_button}>
-              <Button text="Iniciar Sesion" action={handleLogin} styleSet={true} />
+            <div>
+                <Footer />
             </div>
-
-          </div>
-          <div onClick={()=>{
-            navigate("/signin")
-          }}>
-            <p className={styles.navText}>No tengo cuenta</p>
-          </div>
         </div>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
-  )
+    )
 }
--- vetra/src/components/screens/singin/SignIn.tsx
+++ vetra/src/components/screens/singin/SignIn.tsx
@@ -1,104 +1,104 @@
-import styles from "./Singin.module.css"
 import { useState, type ChangeEvent } from "react"
-import { redirect, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { createUser } from "../../../http/userRequest"
+import type { IRegisterBody } from "../../../types/IRegisterBody"
+import { ROLE, type IUser } from "../../../types/IUser"
 import { Button } from "../../ui/Button/Button"
 import { Footer } from "../../ui/Footer/Footer"
-import type { IRegisterBody } from "../../../types/IRegisterBody"
-import { createUser } from "../../../http/userRequest"
-import { ROLE, type IUser } from "../../../types/IUser"
+import styles from "./Singin.module.css"
 
 
 
 const initialValues = {
-  name: "",
-  surname:"",
-  email: "",
-  password: "",
-  confirmPassword: ""
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
 }
 export const SingIn = () => {
-  const [singInInfo, setsingInInfo] = useState<IRegisterBody>(initialValues)
-  const [errorMessage, setErrorMessage] = useState<string>("")
+    const [singInInfo, setsingInInfo] = useState<IRegisterBody>(initialValues)
+    const [errorMessage, setErrorMessage] = useState<string>("")
 
-  const handleChangeInputs = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = event.target
-    setsingInInfo((prev) => ({ ...prev, [`${name}`]: value }))
-  }
-  const navigate = useNavigate()
-  const handleRegister = async () => {
-    if (singInInfo.password != singInInfo.confirmPassword) {
-      setErrorMessage("Las contraseñas no coinciden")
-      return
+    const handleChangeInputs = (event: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = event.target
+        setsingInInfo((prev) => ({ ...prev, [`${name}`]: value }))
+    }
+    const navigate = useNavigate()
+    const handleRegister = async () => {
+        if (singInInfo.password != singInInfo.confirmPassword) {
+            setErrorMessage("Las contraseñas no coinciden")
+            return
+        }
+
+        const newUser: IUser = {
+            nombre: singInInfo.name,
+            apellido: singInInfo.surname,
+            email: singInInfo.email,
+            password: singInInfo.password,
+            rol: ROLE[1],
+            direcciones: []
+        }
+        console.log(newUser)
+        const response = await createUser(newUser)
+        console.log(response)
+        if (response != 201) {
+            setErrorMessage("Ocurrio un error intenta denuevo")
+            return
+        }
+        navigate('/login')
     }
 
-    const newUser: IUser = {
-      nombre: singInInfo.name,
-      apellido: singInInfo.surname,
-      email: singInInfo.email,
-      password: singInInfo.password,
-      rol: ROLE[1],
-      direcciones: []
-    }
-    console.log(newUser)
-    const response = await createUser(newUser)
-    console.log(response)
-    if (response != 201) {
-      setErrorMessage("Ocurrio un error intenta denuevo")
-      return
-    }
-    navigate('/login')
-  }
 
 
 
 
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.contentContainer}>
+                <div className={styles.imageContainer}>
+                    <img className={styles.image} src="https://res.cloudinary.com/danzaburou/image/upload/v1749489285/vetra_banner_wflubd.png" />
+                </div>
+                <div className={styles.logInComponent}>
+                    <div className={styles.logInComponent_content}>
+                        {errorMessage !== "" ? <div className={styles.errorMessage}>{errorMessage}</div> : <div></div>}
+                        <div className={styles.logInComponent_content_inputs}>
+                            <div className={styles.inputComponent}>
+                                <p>Nombre</p>
+                                <input placeholder="John" className={styles.inpuntStyles} type="text" name="name" onChange={handleChangeInputs} />
+                            </div>
+                            <div className={styles.inputComponent}>
+                                <p>Apellido</p>
+                                <input placeholder="Doe" className={styles.inpuntStyles} type="text" name="surname" onChange={handleChangeInputs} />
+                            </div>
+                            <div className={styles.inputComponent}>
+                                <p>Email</p>
+                                <input placeholder="example@gmail.com" className={styles.inpuntStyles} type="text" name="email" onChange={handleChangeInputs} />
+                            </div>
+                            <div className={styles.inputComponent}>
+                                <p>Contraseña</p>
+                                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="password" onChange={handleChangeInputs} />
+                            </div>
+                            <div className={styles.inputComponent}>
+                                <p>Confirmar Contraseña</p>
+                                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="confirmPassword" onChange={handleChangeInputs} />
+                            </div>
+                        </div>
+                        <div className={styles.logInComponent_content_button}>
+                            <Button text="Crear Cuenta" action={handleRegister} styleSet={true} />
+                        </div>
 
-  return (
-    <div className={styles.mainContainer}>
-      <div className={styles.contentContainer}>
-        <div className={styles.imageContainer}>
-          <img className={styles.image} src="public\vetra_banner.png" />
-        </div>
-        <div className={styles.logInComponent}>
-          <div className={styles.logInComponent_content}>
-            {errorMessage !== "" ? <div className={styles.errorMessage}>{errorMessage}</div> : <div></div>}
-            <div className={styles.logInComponent_content_inputs}>
-              <div className={styles.inputComponent}>
-                <p>Nombre</p>
-                <input placeholder="John" className={styles.inpuntStyles} type="text" name="name" onChange={handleChangeInputs} />
-              </div>
-              <div className={styles.inputComponent}>
-                <p>Apellido</p>
-                <input placeholder="Doe" className={styles.inpuntStyles} type="text" name="surname" onChange={handleChangeInputs} />
-              </div>
-              <div className={styles.inputComponent}>
-                <p>Email</p>
-                <input placeholder="example@gmail.com" className={styles.inpuntStyles} type="text" name="email" onChange={handleChangeInputs} />
-              </div>
-              <div className={styles.inputComponent}>
-                <p>Contraseña</p>
-                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="password" onChange={handleChangeInputs} />
-              </div>
-              <div className={styles.inputComponent}>
-                <p>Confirmar Contraseña</p>
-                <input placeholder="examplepassword" className={styles.inpuntStyles} type="password" name="confirmPassword" onChange={handleChangeInputs} />
-              </div>
+                    </div>
+                    <div onClick={() => {
+                        navigate("/login")
+                    }}>
+                        <p className={styles.navText}>Ya tengo cuenta</p>
+                    </div>
+                </div>
             </div>
-            <div className={styles.logInComponent_content_button}>
-              <Button text="Crear Cuenta" action={handleRegister} styleSet={true} />
+            <div>
+                <Footer />
             </div>
-
-          </div>
-          <div onClick={() => {
-            navigate("/login")
-          }}>
-            <p className={styles.navText}>Ya tengo cuenta</p>
-          </div>
         </div>
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
-  )
+    )
 }
